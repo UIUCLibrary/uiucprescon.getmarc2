ca3ee0224e48ba62552d6b6ba111f303530b97a6
henry <henryborchers@yahoo.com>
2020-07-20 16:30:49 -0500

    add tox support

--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ from setuptools import setup
 setup(
     packages=['uiucprescon.getalmarc2'],
     test_suite="tests",
+    tests_require=['pytest'],
     namespace_packages=["uiucprescon"],
     setup_requires=['pytest-runner'],
+    install_requires=["lxml", "requests"],
+
 )
