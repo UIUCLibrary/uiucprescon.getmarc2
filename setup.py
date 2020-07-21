18c914afc16bbb7de9a7b557c6d82ce5a616ac5b
Henry Borchers <hborcher@illinois.edu>
2020-07-21 18:07:16 -0500

    Add namespace decs (#6)
    * Add record namespaces and validation
    
    * Pydocstyle
    
    * typo
    
    * include_package_data
    
    * include package data explicitly

--- setup.py
+++ setup.py
@@ -6,6 +6,9 @@ setup(
     tests_require=['pytest'],
     namespace_packages=["uiucprescon"],
     setup_requires=['pytest-runner'],
+    package_data={
+        "uiucprescon.getalmarc2": ["MARC21slim.xsd"]
+    },
     install_requires=["lxml", "requests"],
 
 )
