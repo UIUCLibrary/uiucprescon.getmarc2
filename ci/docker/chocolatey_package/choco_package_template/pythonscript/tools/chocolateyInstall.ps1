66db7fb86d0d85bf0e0c088949f0c8dbd384bb68
henry <henryborchers@yahoo.com>
2020-08-18 17:37:20 -0500

    install wheel

--- ci/docker/chocolatey_package/choco_package_template/pythonscript/tools/chocolateyInstall.ps1
+++ ci/docker/chocolatey_package/choco_package_template/pythonscript/tools/chocolateyInstall.ps1
@@ -21,6 +21,7 @@ $packageSourceUrl =  '[[PackageSourceUrl]]'
 Write-Host "Creating Python virtualenv at $installDir\venv"
 python -m venv $installDir\venv
 & "$installDir\venv\Scripts\python.exe" -m pip install pip --upgrade --no-compile
+& "$installDir\venv\Scripts\python.exe" -m pip install wheel --upgrade --no-compile
 & "$installDir\venv\Scripts\python.exe" -m pip install $fileLocation --no-compile
 
 $files = get-childitem $installDir -include *.exe -recurse
