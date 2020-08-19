201295460fe4d175e6b6d9e177d68818d26b2bcf
henry <henryborchers@yahoo.com>
2020-08-19 14:36:45 -0500

    install python located with AppInstallLocation

--- ci/docker/chocolatey_package/choco_package_template/pythonscript/tools/chocolateyInstall.ps1
+++ ci/docker/chocolatey_package/choco_package_template/pythonscript/tools/chocolateyInstall.ps1
@@ -12,7 +12,7 @@ $ErrorActionPreference = 'Stop'; # stop on all errors
 [[AutomaticPackageNotesInstaller]]
 $packageName  = '[[PackageName]]'
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$installDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$installDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $fileLocation = Join-Path $toolsDir '[[InstallerFile]]'
 $packageSourceUrl =  '[[PackageSourceUrl]]'
@@ -21,7 +21,7 @@ $PYTHON = "$(Join-Path $(Get-AppInstallLocation('python')) 'python.exe')"
 
 
 Write-Host "Creating Python virtualenv at $installDir\venv"
-$PYTHON -m venv $installDir\venv
+& "$PYTHON" -m venv $installDir\venv
 & "$installDir\venv\Scripts\python.exe" -m pip install pip --upgrade --no-compile
 & "$installDir\venv\Scripts\python.exe" -m pip install $fileLocation
 
