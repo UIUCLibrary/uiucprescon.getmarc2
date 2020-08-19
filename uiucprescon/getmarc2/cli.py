f449c9bddc517e14de2fec6053d2408d72995123
henry <henryborchers@yahoo.com>
2020-08-19 15:56:08 -0500

    doc style

--- uiucprescon/getmarc2/cli.py
+++ uiucprescon/getmarc2/cli.py
@@ -12,9 +12,7 @@ def get_arg_parse() -> argparse.ArgumentParser:
     """Get the CLI parser factory.
 
     Returns: parser
-
     """
-
     parser = argparse.ArgumentParser(description='Get Marc XML data.')
     parser.add_argument("--bibid")
     parser.add_argument("--alma-apikey", required=True)
