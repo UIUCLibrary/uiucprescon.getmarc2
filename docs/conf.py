39e1b82c283c152f1fa132fd6c9e1a68401aa49b
henry <henryborchers@yahoo.com>
2023-01-27 14:24:32 -0600

    docs config tries importing read_configuration from setuptools.config.setupcfg

--- docs/conf.py
+++ docs/conf.py
@@ -15,7 +15,10 @@
 import os
 import re
 import sys
-from setuptools.config import read_configuration
+try:
+    from setuptools.config.setupcfg import read_configuration
+except ImportError:
+    from setuptools.config import read_configuration
 
 sys.path.insert(0, os.path.abspath('..'))
 
