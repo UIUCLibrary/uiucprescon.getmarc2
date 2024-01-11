1db58c44c51d676cc9a866e76cb7cee571a72491
henry <henryborchers@yahoo.com>
2024-01-11 14:44:29 -0600

    lxml<5.1.0 on m1 mac for python 3.8

--- setup.py
+++ setup.py
@@ -13,7 +13,8 @@ setup(
         ]
     },
     install_requires=[
-        "lxml",
+        "lxml<5.1.0; sys_platform == 'darwin' and python_version == '3.8' and platform_machine == 'arm64'",
+        "lxml; sys_platform != 'darwin' or python_version != '3.8' or platform_machine != 'arm64'",
         'importlib-metadata;python_version<"3.8"',
         'importlib-resources;python_version<"3.9"',
         "requests",
